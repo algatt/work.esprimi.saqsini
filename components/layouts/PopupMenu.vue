723176e7b3497a069f90e867aafe717f45590305
Alan Gatt <alangatt@gmail.com>
2020-12-08 19:52:13 +0100

    Popup menu changes direction automatically

--- components/layouts/PopupMenu.vue
+++ components/layouts/PopupMenu.vue
@@ -1,25 +1,23 @@
 <template>
-  <div id="popupMenuRoot" class="relative shadow-lg">
+  <div id="popupMenuRoot" class="relative">
     <button
       class="relative flex items-center justify-center"
       :class="$slots.menuButton ? null : 'btn-link-rounded'"
-      @click.stop="showMenu = true"
+      @click.stop="changeShowMenu"
     >
       <slot name="menuButton"></slot>
       <i v-if="!$slots.menuButton" class="fas fa-ellipsis-v fa-fw"></i>
     </button>
-    <button
-      v-if="showMenu"
-      class="flex flex-col absolute"
-      :style="styleForMenu"
-    >
+    <button v-if="showMenu" class="absolute bg-blue-300 min-w-max">
       <div
         id="popupMenuContainer"
-        class="z-20 bg-white flex flex-col rounded shadow-lg border border-gray-200 w-full"
+        class="absolute flex flex-col z-20 bg-white border-gray-200 rounded shadow min-w-max"
+        :style="styleForMenu"
         @click.stop="closeMenu"
       >
         <slot name="menuItems"></slot>
       </div>
+
       <div
         class="fixed top-0 left-0 w-full h-full z-10 cursor-default"
         @click.stop="closeMenu"
@@ -51,31 +49,75 @@ export default {
   data() {
     return {
       showMenu: false,
+      styleForMenu: null,
     }
   },
   computed: {
-    styleForMenu() {
-      const popupMenuRootWidth = document.getElementById('popupMenuRoot')
-        .clientWidth
-
-      const width = `width: ${this.width}px`
-      let position = `left: ${-Math.abs(popupMenuRootWidth - this.width) / 2}px`
-      if (this.direction === 'left') position = `right: ${5}px`
-      else if (this.direction === 'right') position = `left: ${5}px`
-
-      // const position =
-      //   this.direction === 'left' ? `right: ${5}px` : `left: ${5}px`
-      const top = `top: ${30}px`
-      return `${width}; ${position}; ${top}`
-    },
+    // styleForMenu() {
+    // const popupMenuRootWidth = document.getElementById('popupMenuRoot')
+    //   .clientWidth
+    //
+    // const width = `width: ${this.width}px`
+    // let position = `left: ${-Math.abs(popupMenuRootWidth - this.width) / 2}px`
+    // if (this.direction === 'left') position = `right: ${5}px`
+    // else if (this.direction === 'right') position = `left: ${5}px`
+    //
+    // // const position =
+    // //   this.direction === 'left' ? `right: ${5}px` : `left: ${5}px`
+    // const top = `top: ${30}px`
+    // return `${width}; ${position}; ${top}`
+    // if (!this.showMenu) return null
+    //
+    // const rect = document
+    //   .getElementById('popupMenuContainer')
+    //   .getBoundingClientRect()
+    // console.log(rect)
+    //   const right = rect.right
+    //   const width = rect.width
+    //   const windowWidth = window.innerWidth
+    //   if (width + right > windowWidth) {
+    //     position = `right : 0px`
+    //   } else {
+    //     position = `left: 0px`
+    //   }
+    //   return position
+    // },
   },
   beforeDestroy() {
     this.$emit('closeMenu')
   },
   methods: {
     closeMenu() {
-      this.showMenu = false
+      this.changeShowMenu()
       this.$emit('closeMenu')
+    },
+    changeShowMenu() {
+      this.showMenu = !this.showMenu
+
+      this.$nextTick(() => {
+        if (this.showMenu) {
+          const rect = document
+            .getElementById('popupMenuContainer')
+            .getBoundingClientRect()
+          const right = rect.right
+          const bottom = rect.bottom
+          const windowWidth = window.innerWidth
+          const windowHeight = window.innerHeight
+          console.log(bottom)
+          if (right > windowWidth) {
+            this.styleForMenu = `left : -${right - windowWidth + 25}px`
+          } else {
+            this.styleForMenu = `left: 0px`
+          }
+          if (bottom > windowHeight) {
+            this.styleForMenu += `;bottom: ${35}px`
+          } else {
+            this.styleForMenu += `;top: 0px`
+          }
+        }
+      })
+
+      if (!this.showMenu) this.styleForMenu = null
     },
   },
 }
--- components/surveys/NewQuestionToolBar.vue
+++ components/surveys/NewQuestionToolBar.vue
@@ -1,32 +1,32 @@
 <template>
   <div class="flex items-center justify-center">
-    <div class="flex space-x-0 border border-primary-darker shadow">
-      <popup-menu-vue v-if="showActions" direction="center" :width="128">
+    <div class="flex space-x-0 shadow">
+      <popup-menu-vue v-if="showActions" direction="center">
         <template v-slot:menuButton
-          ><span class="bg-primary text-white font-semibold py-1 w-32"
+          ><span class="bg-primary text-white font-semibold py-1 rounded-l px-3"
             >Actions<i class="fas fa-caret-down fa-fw ml-1"></i></span
         ></template>
         <template v-slot:menuItems>
-          <button @click="$emit('editQuestion')">
+          <button class="w-full" @click="$emit('editQuestion')">
             <span class="popup-menu-button">
               <i class="fa-fw fas fa-pencil-alt fa-sm"></i>Edit</span
             >
           </button>
-          <button @click="$emit('moveQuestion')">
+          <button class="w-full" @click="$emit('moveQuestion')">
             <span class="popup-menu-button">
               <i class="fa-fw fas fa-arrows-alt-v fa-sm"></i>Move</span
             >
           </button>
-          <button @click="$emit('deleteQuestion')">
+          <button class="w-full" @click="$emit('deleteQuestion')">
             <span class="popup-menu-button">
               <i class="fa-fw fas fa-trash-alt fa-sm"></i>Delete</span
             >
           </button>
         </template>
       </popup-menu-vue>
-      <popup-menu-vue direction="center" :width="256">
+      <popup-menu-vue direction="center">
         <template v-slot:menuButton
-          ><span class="bg-primary text-white font-semibold py-1 w-32"
+          ><span class="bg-primary text-white font-semibold py-1 px-3 rounded-r"
             >New Question<i class="fas fa-caret-down fa-fw ml-1"></i></span
         ></template>
         <template v-slot:menuItems>
