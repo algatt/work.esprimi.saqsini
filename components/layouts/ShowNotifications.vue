091c583d35e259e416d4513b54c277e7b049475e
Alan Gatt <alangatt@gmail.com>
2020-12-11 10:23:09 +0100

    Added link to survey in notifications

--- components/layouts/ShowNotifications.vue
+++ components/layouts/ShowNotifications.vue
@@ -4,6 +4,16 @@
     <h5>{{ notification.author.email }}</h5>
     <p class="mt-3">{{ notification.subject }}</p>
     <p class="mt-2 mb-3">{{ notification.content }}</p>
+    <p v-if="notification.linkedEntityTypeFlags.includes('SURVEY')">
+      <nuxt-link
+        :to="{
+          name: 'questions-id',
+          params: { id: notification.linkedEntityCode },
+        }"
+        class="btn-primary px-3"
+        >Go To Survey</nuxt-link
+      >
+    </p>
     <edit-object-modal-bottom-part
       :form="{}"
       which="notifications"
