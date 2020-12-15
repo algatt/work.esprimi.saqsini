657d1565b8bb7a57e8a77df790227ff7a969643b
Alan Gatt <alangatt@gmail.com>
2020-12-15 13:51:04 +0100

    New Font

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
       {
         rel: 'stylesheet',
         href:
-          'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&family=Quicksand:wght@400;500;600;700&display=swap',
+          'https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&family=Quicksand:wght@400;500;600;700&display=swap',
       },
     ],
     script: [
