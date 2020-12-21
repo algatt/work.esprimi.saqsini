74474ea14d92ca67799fb67f44990d00a9573a86
Alan Gatt <alangatt@gmail.com>
2020-12-21 11:43:49 +0100

    Revised branching

--- components/surveys/QuestionBranching.vue
+++ components/surveys/QuestionBranching.vue
@@ -4,162 +4,112 @@
       <p>Currently, this question has no branching.</p>
     </div>
     <div v-else class="mb-3">
-      <label class="label">Existing Branching</label>
-      <toggle-switch
-        class="my-3"
-        :checked="allMustBeMet"
-        :change-colour="false"
-        @clicked="allMustBeMet = $event"
-      >
+      <toggle-switch>
+        class="my-3" :checked="allMustBeMet" :change-colour="false"
+        @clicked="allMustBeMet = $event" >
         <template v-slot:leftLabel>Match any</template>
         <template v-slot:rightLabel>Match all</template>
       </toggle-switch>
       <div
         v-for="(condition, index) in conditions"
-        :key="index"
-        class="flex items-center justify-between px-3 py-2 border border-gray-100 my-1 rounded w-full md:w-10/12 shadow-sm"
+        :key="'condition' + index"
+        class="border border-gray-200 px-3 py-2 shadow my-2 rounded flex w-full items-center"
+        @click="selectedFilter = condition.parentObject.code"
       >
-        <div v-if="condition.questionCode" class="flex flex-grow flex-col">
-          <p>{{ condition.questionText }}</p>
-          <p>{{ condition.answer }}</p>
+        <div
+          v-if="condition.type === 'question'"
+          class="flex flex-col flex-grow"
+        >
+          <div class="flex flex-col font-semibold mb-2 cursor-pointer">
+            {{ condition.parentObject.ordinalPosition }} -
+            {{ condition.parentObject.name }}
+          </div>
+          <div>
+            <span
+              v-for="(option, index2) in condition.condition"
+              :key="'option' + index + '' + index2"
+              class="bg-gray-100 rounded px-2 py-0.5 mr-2"
+              >{{ option }}</span
+            >
+          </div>
         </div>
-        <div v-else>
-          <p>{{ condition.filterType }}</p>
-          <p v-if="condition.filter.name">{{ condition.filter.name }}</p>
-          <p v-else>{{ condition.filter.displayName }}</p>
+        <div v-else class="flex flex-col flex-grow">
+          <div class="flex flex-col font-semibold mb-2">
+            {{ condition.parentObject.text }}
+          </div>
+          <div>
+            <span
+              v-for="(option, index2) in condition.condition"
+              :key="'option' + index + '' + index2"
+              class="bg-gray-100 rounded px-2 py-0.5 mr-2"
+              >{{ option }}</span
+            >
+          </div>
         </div>
-        <div class="flex items-center">
-          <button class="btn-link-danger" @click="deleteCondition(index)">
-            <i class="far fa-trash-alt fa-sm fa-fw"></i>
+        <div class="w-8">
+          <button
+            class="btn-link-danger"
+            @click.stop="deleteCondition(condition.parentObject.code)"
+          >
+            <i class="far fa-trash-alt fa-fw"></i>
           </button>
         </div>
       </div>
     </div>
 
-    <toggle-switch
-      class="my-3"
-      :checked="isDemographicBranching"
-      :change-colour="false"
-      @clicked="isDemographicBranching = $event"
-    >
-      <template v-slot:label
-        >Branching by
-        <popup-info
-          ><template v-slot:text
-            >You can choose to add conditions either by previous responses or
-            contact details.</template
-          ></popup-info
-        ></template
+    <label class="label">Add Branching</label>
+    <select v-model="selectedFilter" class="input mb-2">
+      <option
+        value=""
+        class="bg-gray-100 font-semibold text-gray-800"
+        disabled
+        selected
       >
-      <template v-slot:leftLabel>Survey</template>
-      <template v-slot:rightLabel>Contact Book</template>
-    </toggle-switch>
-    <div v-if="!isDemographicBranching" class="flex flex-wrap items-center">
-      <template v-if="previousQuestions.length !== 0">
-        <div class="w-full flex flex-wrap items-center mb-2">
-          <label class="label md:w-2/12 w-full">Question</label>
-          <select
-            v-model="selectedQuestion"
-            class="input select md:w-8/12 w-full"
-            @change="updateSelectAnswers"
-          >
-            <option
-              v-for="previous in previousQuestionsText"
-              :key="previous.code"
-              :value="previous.code"
-            >
-              {{ previous.text }}
-            </option>
-          </select>
-        </div>
-        <div class="flex items-center w-full flex-wrap mb-2">
-          <label class="label md:w-2/12 w-full">Response</label>
-          <select
-            v-model="selectedAnswer"
-            class="input select md:w-8/12 w-full"
-          >
-            <option
-              v-for="(answer, index) in previousQuestionAnswers"
-              :key="answer + index"
-              :value="answer.value"
-            >
-              {{ answer.value }}
-            </option>
-          </select>
-          <div
-            class="flex w-full md:w-2/12 justify-center items-center px-3 mt-1 md:mt-0"
-          >
-            <button
-              class="btn-primary px-3"
-              :disabled="checkIfDisabledQuestion()"
-              @click="addConditionQuestion"
-            >
-              Add
-            </button>
-          </div>
-        </div>
-      </template>
-      <div v-else class="w-full flex items-center">
-        Previous questions cannot be used for branching.
+        Choose question or contact book value
+      </option>
+      <option
+        v-for="item in combinedQuestionsAndContactText"
+        :key="item.code"
+        :code="item.code"
+        :value="item.code"
+        :class="
+          item.type === 'header'
+            ? 'bg-gray-100 font-semibold text-gray-800'
+            : null
+        "
+        :disabled="item.type === 'header'"
+      >
+        {{ item.text }}
+      </option>
+    </select>
+    <div
+      v-for="item in combinedQuestionAndContactOptions"
+      :key="item.code"
+      class="flex w-full px-3 py-1"
+    >
+      <div class="w-8">
+        <button class="focus:outline-none cursor-pointer">
+          <i
+            class="far fa-check-circle transition-all duration-300 ease-in"
+            :class="checkIfExists(item) ? 'text-primary' : 'text-gray-300'"
+            @click="addCondition(item)"
+          ></i>
+        </button>
+      </div>
+      <div class="flex flex-grow cursor-pointer" @click="addCondition(item)">
+        {{ item.value }}
       </div>
     </div>
-    <template v-else
-      ><div class="flex flex-wrap items-center w-full mb-2">
-        <div class="flex w-full flex-wrap w-full md:w-10/12">
-          <select
-            v-model="selectedFilter"
-            class="input select w-full"
-            @change="selectedFilterItem = filterItems[0].code"
-          >
-            <option v-for="(item, index) in filterKeys" :key="index">
-              {{ item }}
-            </option>
-          </select>
-        </div>
-        <div class="flex flex-wrap items-center w-full mt-2">
-          <select
-            v-model="selectedFilterItem"
-            class="input select w-10/12"
-            :disabled="!filterItems || filterItems.length === 0"
-          >
-            <option
-              v-for="(item, index) in filterItems"
-              :key="index"
-              :value="item.code"
-            >
-              <template v-if="selectedFilter !== 'Contacts'">{{
-                item.name
-              }}</template>
-              <template v-else-if="selectedFilter === 'Contacts'">{{
-                item.displayName
-              }}</template>
-            </option>
-          </select>
-          <div
-            class="flex w-full md:w-2/12 justify-center items-center mt-2 md:mt-0"
-          >
-            <button
-              class="btn-primary px-3"
-              :disabled="checkIfDisabledDemographic()"
-              @click="addConditionDemographic"
-            >
-              Add
-            </button>
-          </div>
-        </div>
-      </div></template
-    >
   </div>
 </template>
 
 <script>
 import { parseQuestionToForm } from '~/helpers/parseSurveyObjects'
 import ToggleSwitch from '~/components/layouts/ToggleSwitch'
-import PopupInfo from '~/components/layouts/PopupInfo'
 
 export default {
   name: 'QuestionBranching',
-  components: { PopupInfo, ToggleSwitch },
+  components: { ToggleSwitch },
   props: {
     existingConditions: {
       type: Object,
@@ -168,14 +118,10 @@ export default {
   },
   data() {
     return {
-      selectedQuestion: null,
-      selectedAnswer: null,
-      isDemographicBranching: false,
       allMustBeMet: false,
       conditions: [],
       filters: {},
-      selectedFilter: null,
-      selectedFilterItem: null,
+      selectedFilter: '',
     }
   },
   computed: {
@@ -199,30 +145,55 @@ export default {
       })
     },
     previousQuestionsText() {
-      return this.previousQuestions.map((el) => {
+      const x = this.previousQuestions.map((el) => {
         const text = this.getQuestionText(el)
-        return { code: el.code, text: text.name + ' - ' + text.text }
+        return {
+          code: el.code,
+          text: text.name + ' - ' + text.text,
+          type: 'question',
+          obj: el,
+        }
       })
+      x.unshift({ code: -1, text: 'Questions', type: 'header' })
+      return x
     },
     previousQuestionAnswers() {
+      if (isNaN(Number(this.selectedFilter))) return []
       const question = this.questions.find((el) => {
-        return el.code === this.selectedQuestion
+        return el.code === this.selectedFilter
       })
       if (!question) return null
       return question.options.map((el) => {
-        return { value: el.value }
+        return { value: el.value, obj: el }
       })
     },
-    filterKeys() {
+    contactText() {
       const x = Object.keys(this.filters)
-      const result = []
+      const result = [{ code: -2, text: 'Contact Book', type: 'header' }]
       x.forEach((el) => {
-        result.push(el.charAt(0).toUpperCase() + el.slice(1))
+        result.push({
+          code: 'contact ' + el,
+          text: el.charAt(0).toUpperCase() + el.slice(1),
+          type: 'contact',
+          obj: {
+            code: 'contact ' + el,
+            text: el.charAt(0).toUpperCase() + el.slice(1),
+          },
+        })
       })
       return result
     },
-    filterItems() {
-      let x = this.filters[this.selectedFilter.toLowerCase()]
+    contactOptions() {
+      if (
+        !isNaN(Number(this.selectedFilter)) ||
+        this.selectedFilter.indexOf('contact ') !== 0
+      )
+        return []
+
+      let which = this.selectedFilter.toLowerCase()
+      which = which.replace('contact ', '')
+      let x = this.filters[which]
+
       const fieldToSort =
         this.selectedFilter.toLowerCase() === 'contacts'
           ? 'displayName'
@@ -232,8 +203,27 @@ export default {
           ? 1
           : -1
       })
-      x.unshift({ [fieldToSort]: 'All', value: 'ALL' })
+      x = x.map((el) => {
+        return {
+          code: el.code,
+          value: el[fieldToSort],
+          obj: el,
+        }
+      })
+      x.unshift({ code: 'all', value: 'All' })
       return x
+    },
+
+    combinedQuestionsAndContactText() {
+      return this.previousQuestionsText.concat(this.contactText)
+    },
+    combinedQuestionAndContactOptions() {
+      if (
+        !isNaN(Number(this.selectedFilter)) ||
+        this.selectedFilter.indexOf('contact ') !== 0
+      )
+        return this.previousQuestionAnswers
+      return this.contactOptions
     },
   },
   watch: {
@@ -248,70 +238,67 @@ export default {
     },
   },
   async created() {
-    if (this.previousQuestionsText && this.previousQuestionsText.length > 0) {
-      this.selectedQuestion = this.previousQuestionsText[0].code
-      this.selectedAnswer = this.previousQuestionAnswers[0].value
-    }
     this.conditions = this.existingConditions.rules
     this.allMustBeMet = this.existingConditions.allMustBeMet
-
     this.filters = await this.$store.dispatch('invitations/getFilters')
-    this.selectedFilter = this.filterKeys[0]
-    this.selectedFilterItem =
-      this.filterItems.length > 0 ? this.filterItems[0].code : null
   },
   methods: {
+    checkIfExists(item) {
+      const existingCondition = this.conditions.find((el) => {
+        return el.parentObject.code === this.selectedFilter
+      })
+      if (existingCondition)
+        return existingCondition.condition.includes(item.value)
+      return false
+    },
+    addCondition(item) {
+      const parentObject = this.combinedQuestionsAndContactText.find((el) => {
+        return el.code === this.selectedFilter
+      })
+
+      const existingCondition = this.conditions.find((el) => {
+        return el.parentObject.code === this.selectedFilter
+      })
+
+      let values = []
+      if (parentObject.type === 'contact' && item.value === 'All') {
+        values = this.filters[parentObject.code.replace('contact ', '')]
+        values = values.map((el) => {
+          return el.name
+        })
+      }
+
+      if (!existingCondition)
+        this.conditions.push({
+          type: parentObject.type,
+          parentObject: parentObject.obj,
+          condition: values.length === 0 ? [item.value] : values,
+        })
+      else if (
+        existingCondition.condition.includes(item.value) &&
+        values.length === 0
+      ) {
+        existingCondition.condition = existingCondition.condition.filter(
+          (el) => {
+            return el !== item.value
+          }
+        )
+        if (existingCondition.condition.length === 0)
+          this.conditions = this.conditions.filter((el) => {
+            return el.parentObject.code !== this.selectedFilter
+          })
+      } else if (values.length !== 0) existingCondition.condition = values
+      else existingCondition.condition.push(item.value)
+    },
     getQuestionText(question) {
       return parseQuestionToForm(question)
     },
-    updateSelectAnswers() {
-      this.selectedAnswer = this.previousQuestionAnswers[0].value
-    },
-    checkIfDisabledQuestion() {
-      return (
-        this.conditions.filter((el) => {
-          return (
-            el.questionCode === this.selectedQuestion &&
-            el.answer === this.selectedAnswer
-          )
-        }).length > 0
-      )
-    },
-    checkIfDisabledDemographic() {
-      return (
-        this.conditions.filter((el) => {
-          return (
-            el.filterType === this.selectedFilter &&
-            el.filter.code === this.selectedFilterItem
-          )
-        }).length > 0
-      )
-    },
-    addConditionQuestion() {
-      const questionText = this.getQuestionText(
-        this.questions.find((el) => {
-          return el.code === this.selectedQuestion
-        })
-      ).text
-      this.conditions.push({
-        questionCode: this.selectedQuestion,
-        questionText,
-        answer: this.selectedAnswer,
+
+    deleteCondition(code) {
+      this.conditions = this.conditions.filter((el) => {
+        return el.parentObject.code !== code
       })
-    },
-    addConditionDemographic() {
-      this.conditions.push({
-        filterType: this.selectedFilter,
-        filter: this.filters[this.selectedFilter.toLowerCase()].find((el) => {
-          return el.code === this.selectedFilterItem
-        }),
-      })
-    },
-    deleteCondition(index) {
-      this.conditions.splice(index, 1)
     },
   },
 }
 </script>
-
-<style scoped></style>
