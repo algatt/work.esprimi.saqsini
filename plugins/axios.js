0462c2f83121c837b4e9dd1f73b26ea345d6afab
Alan Gatt <alangatt@gmail.com>
2020-12-29 20:56:20 +0100

    changed api key

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,5 +1,5 @@
 export default function ({ $axios, store, redirect }) {
-  $axios.setHeader('Authorization', 'b0ded4a6-f1a8-45c4-aac5-7110ed437333')
+  $axios.setHeader('Authorization', '88920fdb-d8ad-4133-a0da-2580a56d7718')
   $axios.setHeader('Content-Type', 'application/x-www-form-urlencoded', [
     'post',
   ])
