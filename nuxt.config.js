5972e31c3a310ab7d5cbf1856f76c8ba30a8e881
Alan Gatt <alan@lobeslab.com>
2021-05-17 07:42:49 +0200

    attempting to make postcss work
    Took 5 minutes

--- nuxt.config.js
+++ nuxt.config.js
@@ -96,5 +96,14 @@ export default {
   },
 
   // Build Configuration (p)
-  build: {},
+  build: {
+    postcss: {
+      preset: {
+        features: {
+          // Fixes: https://github.com/tailwindcss/tailwindcss/issues/1190#issuecomment-546621554
+          'focus-within-pseudo-class': false,
+        },
+      },
+    },
+  },
 }
